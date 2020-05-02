bf58b36579975da6689c697f5db349b3906abf74
AlexIsTerrified <alexepicity@gmail.com>
2020-05-02 01:53:30 -0400

    Updates

---
+++ precache-manifest.b4a1ab859e79f49bd0f27e1398be20c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd294f633090c6e2d051371c13f948db",
+    "url": "/blog-frontend/index.html"
+  },
+  {
+    "revision": "517e5224fccd46273964",
+    "url": "/blog-frontend/static/css/main.6eeb9b75.chunk.css"
+  },
+  {
+    "revision": "991f2dc237f8a8e9d58c",
+    "url": "/blog-frontend/static/js/2.a84cf133.chunk.js"
+  },
+  {
+    "revision": "40ad9103a287cd122a894efdf2dc0e44",
+    "url": "/blog-frontend/static/js/2.a84cf133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517e5224fccd46273964",
+    "url": "/blog-frontend/static/js/main.6897d1b1.chunk.js"
+  },
+  {
+    "revision": "844d93a34d0d9edb51e4",
+    "url": "/blog-frontend/static/js/runtime-main.05f885d0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
